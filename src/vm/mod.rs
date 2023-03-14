01c05106f25dadb9105f0b92144dfd0e350e139d
Nyk Ma <nykma@mask.io>
2023-03-14 16:37:00 +0800

    [#] vm: add JS runtime support

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,41 +1,32 @@
+use anyhow::Result;
+use std::{io::Write, rc::Rc};
+
 #[cfg(test)]
 mod tests;
 
-use serde::{Serialize, Deserialize};
-use web3::types::{Block, Transaction};
+pub(crate) mod block_call;
 
-#[derive(Debug, Clone)]
-pub struct BlockCallbackRequest {
-    pub chain: BlockCallbackRequestChain
+/// Run given JS code in VM.
+async fn run_js_code(code: &str) -> Result<()> {
+    let tmpdir = tempfile::tempdir()?;
+    let tmpfile_path = tmpdir
+        .path()
+        .join(format!("{}.ts", uuid::Uuid::new_v4().to_string()));
+    let mut tmpfile = std::fs::File::create(tmpfile_path.clone())?;
+    write!(tmpfile, "{}", code).unwrap();
+
+    run_js_file(tmpfile_path.to_str().unwrap()).await
 }
 
-#[derive(Debug, Clone)]
-pub struct BlockCallbackRequestChain {
-    pub name: String,
-    pub chain_id: String,
-}
-
-#[typetag::serde(tag = "type", content = "data")]
-pub trait BlockCallbackResponse {
-    fn action(&self);
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct BlockCallbackResponseWebhook {
-    pub url: String,
-}
-
-#[typetag::serde(name = "webhook")]
-impl BlockCallbackResponse for BlockCallbackResponseWebhook {
-    fn action(&self) {
-        println!("Webhook: {}", self.url); // TODO
-    }
-}
-
-impl TryFrom<Block<Transaction>> for BlockCallbackRequest {
-    type Error = anyhow::Error;
-
-    fn try_from(value: Block<Transaction>) -> Result<Self, Self::Error> {
-        todo!()
-    }
+/// Run a specific JS file in VM.
+async fn run_js_file(file_path: &str) -> Result<()> {
+    let main_module = deno_core::resolve_path(file_path)?;
+    let mut js_runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions {
+        module_loader: Some(Rc::new(deno_core::FsModuleLoader)),
+        ..Default::default()
+    });
+    let mod_id = js_runtime.load_main_module(&main_module, None).await?;
+    let result = js_runtime.mod_evaluate(mod_id);
+    js_runtime.run_event_loop(false).await?;
+    result.await?
 }
--- src/vm/tests.rs
+++ src/vm/tests.rs
@@ -1,11 +1,8 @@
 use super::*;
 use anyhow::Result;
 
-#[test]
-fn test_deserialize_response() -> Result<()> {
-    let response_raw = r#"{"type": "webhook", "data": {"url": "test-url"}}"#;
-    let response: Box<dyn BlockCallbackResponse> = serde_json::from_str(response_raw)?;
-    response.action();
-
-    Ok(())
+#[tokio::test]
+async fn test_run_js_code_success() -> Result<()> {
+    let code = r#"Deno.core.print("Test\n");"#;
+    run_js_code(code).await
 }
